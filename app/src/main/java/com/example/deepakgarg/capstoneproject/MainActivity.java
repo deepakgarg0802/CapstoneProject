a63027e7ae3f8bcc6e7f5601499d307b50c6121e
deepakgarg0802 <deepakgarg0802@gmail.com>
2017-03-22 22:47:45 +0530

    Feat : Sources of news and logo displayed - Volley and Picassa used - fragment code added

--- app/src/main/java/com/example/deepakgarg/capstoneproject/MainActivity.java
+++ app/src/main/java/com/example/deepakgarg/capstoneproject/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            NewsFragment moviesFragment = new NewsFragment();
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_main, moviesFragment)
+                .commit();
+        }
+
     }
 }
